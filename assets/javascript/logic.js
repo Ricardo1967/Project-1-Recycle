a128551f234dcc3a2d36ca7b28d3d62a71776b54
monterocjaime <monterocjaime@gmail.com>
2018-07-05 18:33:24 -0400

    Added variables, and on click event that grabs values from input boxes

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,7 +1,17 @@
-
+//Starts JAVASCRIPT logic, once the DOM is ready, and fully loaded
 
 $(document).ready(function () {
+//Initialize variables to use in the logic.
+var zipCode = 0;
+var materialR = 0;
+var map = 0;
 
-    console.log("this is our JS"); 
+//When the button is clicked, grabs the information from the input boxes
+$("#submitBtn").on("click", function(event){
+event.preventDefault();
+zipCode = $("#zipInput").val().trim();
+materialR = $("#materialInput").val().trim();
+console.log(zipCode, materialR);
+});
 
 });
