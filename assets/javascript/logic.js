7346c20a0768d61918b8dd469df927bb92c826b2
monterocjaime <monterocjaime@gmail.com>
2018-07-06 01:49:00 -0400

    Added 2 functions, one gets geolocation, the other one use geocode to generate map, in progress

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,12 +1,45 @@
 //Starts JAVASCRIPT logic, once the DOM is ready, and fully loaded
+$(document).ready(function () {
+    //Initialize variables to use in the logic.
+    // var zipCode = 0;
+    var materialR = 0;
+    var map = 0;
+    var late;
+    var lnge;
+    //When the button is clicked, grabs the information from the input boxes
+    $("#submitBtn").on("click", function geocode(event){
+            event.preventDefault();
+            var zipCode = $("#zipInput").val().trim();
+    
+            axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+                params: {
+                    address:zipCode,
+                    key:'AIzaSyCewNtEX9C_9wLxAOvKyTmjT8pV0NJYC_o'
+                }
+            })
+            .then(function(response){
+                console.log(response);
+    
+                var formattedAddress = response.data.results[0].formatted_address;
+                late = response.data.results[0].geometry.location.lat;
+                lnge = response.data.results[0].geometry.location.lng;
+            });
+            materialR = $("#materialInput").val().trim();
+            console.log(zipCode, materialR);
+    
+            
+    initMap(late, lnge);
+    
+        }); //Finishes function that grabs input values (WORKING)
+    
+    });
 
 
+function initMap(lt,lg) {
 
-function initMap() {
-    //Map Options
  var options = {
      zoom: 11,
-     center: {lat:25.7617, lng:-80.1918}
+     center: {lat:lt, lng:- lg}
  }
  // New Map
  var map = new google.maps.Map(document.getElementById('map'), options);
@@ -27,22 +60,4 @@ function initMap() {
      infoWindow.open(map, marker);
  } );
 }
-$(document).ready(function () {
-//Initialize variables to use in the logic.
-var zipCode = 0;
-var materialR = 0;
-var map = 0;
 
-//When the button is clicked, grabs the information from the input boxes
-$("#submitBtn").on("click", function(event){
-        event.preventDefault();
-        zipCode = $("#zipInput").val().trim();
-        materialR = $("#materialInput").val().trim();
-        console.log(zipCode, materialR);
-
-        
-
-
-    }); //Finishes function that grabs input values (WORKING)
-
-});
